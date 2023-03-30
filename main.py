added label normalization, fixed/refactored bias mitigation loss, restored metrics to custom, added hdf5 and multi-pickle, added horizontal barplot

main.py
@@ -28,7 +28,7 @@ import optuna
 # Package modules
 from options import *
 from modeling import RepBERT_Train, CODER, get_loss_module
-from dataset import MYMARCO_Dataset, MSMARCODataset
+from dataset import MYMARCO_Dataset
 from dataset import lookup_times, sample_fetching_times, collation_times, retrieve_candidates_times, prep_docids_times
 from optimizers import get_optimizers, MultiOptimizer, get_schedulers, MultiScheduler
 import utils
@@ -64,7 +64,7 @@ def train(args, model, val_dataloader, tokenizer=None, fairrmetric=None, trial=N
                                     max_inj_relevant=args.max_inj_relevant,
                                     collection_neutrality_path=args.collection_neutrality_path)
     collate_fn = train_dataset.get_collate_func(num_random_neg=args.num_random_neg, n_gpu=args.n_gpu,
-                                                label_format=model.loss_module.formatting)
+                                                label_format=model.loss_module.formatting, label_normalization=args.label_normalization)
     logger.info("'train' data loaded in {:.3f} sec".format(time.time() - start_time))
 
     utils.write_list(os.path.join(args.output_dir, "train_IDs.txt"), train_dataset.qids)
@@ -220,10 +220,11 @@ def train(args, model, val_dataloader, tokenizer=None, fairrmetric=None, trial=N
                     avg_batch_time = batch_times.get_average()
                     tb_writer.add_scalar('train/batch time', avg_batch_time, global_step)
                     if model.score_cands.temperature is not None:
-                        tb_writer.add_scalar('train/temperature', model.score_cands.temperature, global_step) 
-
+                        tb_writer.add_scalar('train/temperature', model.score_cands.temperature, global_step)
+                        tb_writer.add_scalar('train/score_bias', model.score_cands.b, global_step) 
+                    
+                    logger.info("Mean loss over {} steps: {:.5f}".format(args.logging_steps, cur_loss))
                     if args.debug:
-                        logger.debug("Mean loss over {} steps: {:.5f}".format(args.logging_steps, cur_loss))
                         for i, s in enumerate(scheduler.get_last_lr()):
                             logger.debug('Learning rate ({}): {}'.format(sstr[i], s))
                         logger.debug("Current memory usage: {} MB".format(int(np.round(utils.get_current_memory_usage()))))
@@ -381,8 +382,8 @@ def evaluate(args, model, dataloader, fairrmetric=None):
     labels_exist = qrels is not None
 
     # num_docs is the (potentially variable) number of candidates per query
-    # relevances = []  # (total_num_queries) list of (num_docs) lists with non-zeros at the indices corresponding to actually relevant passages
-    # num_relevant = []  # (total_num_queries) list of number of ground truth relevant documents per query
+    relevances = []  # (total_num_queries) list of (num_docs) lists with non-zeros at the indices corresponding to actually relevant passages
+    num_relevant = []  # (total_num_queries) list of number of ground truth relevant documents per query
     scores = {}  # dict {str(qID): dict{str(passageid): rel_score}}  # string IDs required for pytrec_eval
     query_time = 0  # average time for the model to score candidates for a single query
     total_loss = 0  # total loss over dataset
@@ -426,17 +427,17 @@ def evaluate(args, model, dataloader, fairrmetric=None):
             # extend results dictionary by batch_size elements. Indexing works both for np.arrays and list of lists formats for _docs and_scores
             scores.update({str(qids[i]): {str(ranksorted_docs[i][j]): sorted_scores[i][j] for j in range(len(ranksorted_docs[i]))} for i in range(len(qids))})
 
-            # if labels_exist:
-            #     relevances.extend(get_relevances(qrels[qids[i]], ranksorted_docs[i], relevant_at_level=dataloader.dataset.relevant_at_level) for i in range(len(qids)))
-            #     # number of g.t. positives in entire dataset for each query
-            #     num_relevant.extend(len([candid for candid in qrels[qid]
-            #                              if qrels[qid][candid] >= dataloader.dataset.relevant_at_level]) for qid in qids)
+            if labels_exist:
+                relevances.extend(get_relevances(qrels[qids[i]], ranksorted_docs[i], relevant_at_level=dataloader.dataset.relevant_at_level) for i in range(len(qids)))
+                # number of g.t. positives in entire dataset for each query
+                num_relevant.extend(len([candid for candid in qrels[qid]
+                                         if qrels[qid][candid] >= dataloader.dataset.relevant_at_level]) for qid in qids)
 
     if labels_exist:
         # try:
-        eval_metrics = get_retrieval_metrics(scores, qrels=dataloader.dataset.reformatted_qrels, cutoff_values=(5, args.metrics_k), 
-                                                 relevance_level=dataloader.dataset.relevant_at_level, verbose=False)  # {metric_name: metric_value}
-        #     # eval_metrics = calculate_metrics(relevances, num_relevant, args.metrics_k) # aggregate metrics for the entire dataset
+        # eval_metrics = get_retrieval_metrics(scores, qrels=dataloader.dataset.reformatted_qrels, cutoff_values=(5, args.metrics_k), 
+        #                                          relevance_level=dataloader.dataset.relevant_at_level, verbose=False)  # {metric_name: metric_value}
+        eval_metrics = calculate_metrics(relevances, num_relevant, args.metrics_k) # aggregate metrics for the entire dataset
         # except Exception as x:
         #     print(x)
         #     traceback.print_stack()
@@ -507,7 +508,7 @@ def get_dataset(args, eval_mode, tokenizer):
                                load_collection_to_memory=args.load_collection_to_memory,
                                inject_ground_truth=args.inject_ground_truth,
                                relevance_labels_mapping=args.relevance_labels_mapping,
-                               include_at_level=args.include_at_level, relevant_at_level=args.relevant_at_level)
+                               include_at_level=args.eval_include_at_level, relevant_at_level=args.eval_relevant_at_level)
 
 
 def get_query_encoder(query_encoder_from, query_encoder_config):
@@ -592,7 +593,6 @@ def setup(args):
     if (not config['no_timestamp']) or (len(config['experiment_name']) == 0):
         rand_suffix = "".join(random.choices(string.ascii_letters + string.digits, k=3))
         output_dir += "_" + formatted_timestamp + "_" + rand_suffix
-    config['output_dir'] = output_dir
     os.makedirs(output_dir, exist_ok=True)
 
     # Save configuration as a (pretty) json file
@@ -601,10 +601,12 @@ def setup(args):
     logger.info("Stored configuration file in '{}'".format(output_dir))
     
     # Rest of configuration (does not need to be saved as a JSON config file)
+    config['output_dir'] = output_dir
     info_dict = {'initial_timestamp': formatted_timestamp,
-                'save_dir': os.path.join(output_dir, 'checkpoints'),
-                'pred_dir': os.path.join(output_dir, 'predictions'),
-                'tensorboard_dir': os.path.join(output_dir, 'tb_summaries')}
+                 'output_dir': output_dir,
+                 'save_dir': os.path.join(output_dir, 'checkpoints'),
+                 'pred_dir': os.path.join(output_dir, 'predictions'),
+                 'tensorboard_dir': os.path.join(output_dir, 'tb_summaries')}
     config.update(info_dict)
     
     args = utils.dict2obj(config)  # Convert config dict to args object

modeling.py
@@ -3,10 +3,11 @@ import logging
 from typing import Optional, Any, Dict, Union
 
 import torch
-import numpy as np
 from torch import nn, Tensor
 import torch.nn.functional as F
 from transformers import BertModel, BertPreTrainedModel, AutoModel
+
+import dataset
 
 logger = logging.getLogger(__name__)
 
@@ -40,7 +41,7 @@ def _mask_both_directions(valid_mask, token_type_ids):
 
 class RepBERT_Train(BertPreTrainedModel):
     """
-    Instead of bi-encoder, a single encoder is used, and token_type_ids with attention masking is applied to separate
+    NOTE(GEO): Instead of bi-encoder, a single encoder is used, and token_type_ids with attention masking is applied to separate
     query self-attention and doc self-attention. There is no query-doc token interaction in self-attention calculations.
     Based on HF BERT implementation, this may be inefficient: attention masks (apparently) are just added, instead
     of preventing computations altogether. This means that the full O(seq_len^2) cost is there, and is therefore worse
@@ -249,14 +250,14 @@ class DotProductScorer(Scorer):
     # when using the model for dense retrieval, i.e. outside of reranking. However, linear transformations don't affect rankings.
     """
     
-    TEMP_INIT = 0  # initial temperature value. Expects exponentiation of temperature parameter!
+    TEMP_INIT = 1 #0  # initial temperature value. Expects exponentiation of temperature parameter!
     
     def __init__(self, scoring_mode='', pre_activation=None, normalize=False, aggregation='mean', temperature: Union[str,float,None]=None):
         """
         :param scoring_mode: string, same as the option used to initialize `CODER`. At this point, the string
             must start with "doc_product" or "cosine", but the suffix can specify a (non)linear transformation to be used on scores
         :param pre_activation: activation function to use on representations BEFORE computing the inner product
-        :param normalize: if True, will divide product by vector norms, i.e. will compute the cosine similarity
+        :param normalize: if True, will divide product by vector L2-norms, i.e. will compute the cosine similarity
         :param aggregation: defines how to aggregate final query token representations to obtain a single vector
             representation for the query. 'mean' will average, 'first' will simply select the first vector.
             'None' will not aggregate token representations
@@ -308,14 +309,14 @@ class DotProductScorer(Scorer):
             scores = torch.matmul(output_emb, agg_query_emb[:, :, None]).squeeze()  # to disable scaling
             
         if self.temperature:
-            scores = scores / torch.exp(self.temperature) + self.b
+            scores = (scores * self.temperature) + self.b  #torch.exp(self.temperature)
 
         return scores
 
 
 class BaseLoss(nn.Module):
 
-    def __init__(self, formatting='scores'):
+    def __init__(self, formatting='scores', **kwargs):
         """
         :param formatting: 'indices' or 'scores'.
             If 'scores', assumes that `labels` have the same formatting as `predictions`:
@@ -393,8 +394,26 @@ class RelevanceCrossEntropyLoss(BaseLoss):
 
 class RelevanceListnetLoss(BaseLoss):
     """
-    KL-divergence loss
+    KL-divergence between the model-predicted score distribution (possibly tempered by temperature) and the target relevance distribution
     """
+
+    def __init__(self, formatting='scores', normalization=None):
+        """
+        :param formatting: 'indices' or 'scores'.
+            If 'scores', assumes that `labels` have the same formatting as `predictions`:
+                each element corresponds to the g.t. relevance score of the respective document (in the order of `predictions`), 
+                with -Inf for non-relevant and padding, e.g. [2.0 1.0 1.0 -Inf ... -Inf]
+            If 'indices', assumes num_relevant integer indices of relevant documents and is padded with -1,
+                e.g. [0, 1, 2, -1, ..., -1]
+        :param normalization: how to normalize scores used as targets (labels): None, 'max', 'maxmin', 'std'. 
+            If None, no normalization is performed; this results in arbitrary range of scores, with potentially flat pre-softmax "distribution".
+            Normalization is supposed to help dealing with the variety of relevance score ranges resulting from different hyperparameter combinations.
+            Ultimately, a KL divergence between the model-predicted score distribution (possibly tempered by temperature)
+            and the target relevance distribution will be computed, which will be obtained by applying a softmax on relevances computed here.
+        """
+        super(RelevanceListnetLoss, self).__init__()
+        self.formatting = formatting
+        self.normalization = normalization
 
     def forward(self, predictions, labels, padding_mask=None):
         """
@@ -412,18 +431,58 @@ class RelevanceListnetLoss(BaseLoss):
             _labels_values[is_padding] = float("-Inf")
         else:
             _labels_values = labels
+            
+        if self.normalization is not None:
+            _labels_values = dataset.normalize_batch(_labels_values, norm_type=self.normalization)
 
         # NOTE: _labels_values = _labels_values / torch.sum(is_relevant, dim=1).unsqueeze(dim=1)
         # is equivalent but interestingly much slower than setting -Inf and computing Softmax; maybe due to CUDA Softmax code
         labels_probs = torch.nn.Softmax(dim=1)(_labels_values)
 
         if padding_mask is not None:
+            # labels don't need to be masked, since they already get -Inf for padding in collate_fn
             predictions[padding_mask] = float("-Inf")
 
         predictions_logprobs = torch.nn.LogSoftmax(dim=1)(predictions)  # (batch, num_cands) log-distribution over docs
         # KLDivLoss expects predictions ('inputs') as log-probabilities and 'targets' as probabilities
         loss = torch.nn.KLDivLoss(reduction='batchmean')(predictions_logprobs, labels_probs)
 
+        return loss
+
+
+class BiasRegularizationLoss(BaseLoss):
+    """KL-divergence between the model-predicted relevance score distribution (possibly tempered by temperature) 
+    and the target neutrality distribution. Essentially the same as RelevanceListnetLoss, 
+    but with a cutoff on the number of considered documents (i.e. the number of documents used to compute the KL-divergence)."""
+    
+    def __init__(self, formatting='indices', bias_regul_cutoff=10):
+        super().__init__(formatting=formatting)
+        self.bias_regul_cutoff = bias_regul_cutoff
+        
+    def forward(self, predictions, labels, padding_mask=None):
+        """
+        :param predictions: (batch_size, num_candidates) relevance scores (arb. range) for each candidate and query.
+        :param labels: (batch_size, num_candidates) tensor of target scores (here, neutrality scores)
+        :param padding_mask: (batch_size, num_candidates) boolean mask. 1 where element is padding, 0 where valid
+        :return: loss: scalar tensor. Mean loss per query
+        """
+        
+        cutoff = min(self.bias_regul_cutoff, labels.shape[1])
+
+        indices_sorted = torch.argsort(predictions, dim=1, descending=True)
+        allow_inds = indices_sorted[:, :cutoff]  # (batch_size, cutoff) indices of `scores` to take into consideration (top-cutoff docs)
+        
+        indices_mask = torch.full_like(predictions, float('-Inf'), dtype=torch.float32)
+        indices_mask[torch.arange(indices_mask.shape[0])[:, None], allow_inds] = 0
+        
+        if padding_mask is not None:
+            # Most likely not necessary, since cut-off will almost certainly exclude padding (that may result from some queries having fewer than num_candidates docs)
+            indices_mask += padding_mask * float('-Inf')
+
+        label_probs = torch.nn.Softmax(dim=1)(labels + indices_mask)
+        prediction_logprobs = torch.nn.LogSoftmax(dim=1)(predictions + indices_mask)
+
+        loss = torch.nn.KLDivLoss(reduction='batchmean')(prediction_logprobs, label_probs)
         return loss
 
 
@@ -530,9 +589,9 @@ class MultiTierLoss(BaseLoss):
                     start_inds.append(new_ind)
         return start_inds
 
-    def forward(self, scores, labels, padding_mask=None):
+    def forward(self, predictions, labels, padding_mask=None):
         """
-        :param scores: (batch_size, num_cands) relevance scores for each candidate document and query.
+        :param predictions: (batch_size, num_cands) relevance scores for each candidate document and query.
         :param labels: (batch_size, num_cands) int tensor which for each query (row) contains the indices (positions) of the
                 relevant documents within its corresponding pool of candidates (cand_inds). If n relevant documents exist,
                 then labels[0:n] are the positions of these documents inside `cand_inds`, and labels[n:] == -1,
@@ -547,22 +606,22 @@ class MultiTierLoss(BaseLoss):
         # is_relevant = (labels > -1)
         # num_relevant = is_relevant.sum(dim=1)  # total number of relevant documents in the batch
         # num_negatives = scores.shape[1] - num_relevant
-        num_negatives = scores.shape[1]  # this is approximately correct; we hereby include the g.t. relevant in tier 1
+        num_negatives = predictions.shape[1]  # this is approximately correct; we hereby include the g.t. relevant in tier 1
         start_inds = self.get_tier_inds(num_negatives)
 
         # Treating as "positives" the documents within each tier, the loss compares
         # their scores to the scores of documents in all lower tiers (and the ones in-between lower tiers)
         # complexity O(num_tiers)
-        loss = torch.zeros(scores.shape[0], dtype=torch.float32, device=scores.device)  # (batch_size,) loss for each query
+        loss = torch.zeros(predictions.shape[0], dtype=torch.float32, device=predictions.device)  # (batch_size,) loss for each query
         for t in range(self.num_tiers - 1):
             # variant to compare with immediately lower tier: inds2 = range(start_inds[t+1], start_inds[t+1] + self.tier_size)
-            loss += self.compute_diffs(scores, range(start_inds[t], start_inds[t] + self.tier_size),
-                                       range(start_inds[t + 1], scores.shape[1]))
+            loss += self.compute_diffs(predictions, range(start_inds[t], start_inds[t] + self.tier_size),
+                                       range(start_inds[t + 1], predictions.shape[1]))
 
         if self.gt_function is not None:
             # in this case a special loss component will be computed from ground truth relevant documents versus
             # all other candidates using the provided function, added with a scaling factor of `self.gt_factor`
-            gt_loss = self.gt_function(scores, labels)
+            gt_loss = self.gt_function(predictions, labels)
             loss = loss + self.gt_factor * gt_loss
 
         if self.reduction == 'mean':
@@ -584,7 +643,7 @@ def get_loss_module(loss_type, args):
     elif loss_type == 'crossentropy':
         return RelevanceCrossEntropyLoss(formatting='indices')
     elif loss_type == 'listnet':
-        return RelevanceListnetLoss(formatting='scores')
+        return RelevanceListnetLoss(formatting='scores', normalization=None)
     elif loss_type == 'multitier':
         return MultiTierLoss(formatting='indices',
                              num_tiers=args.num_tiers, tier_size=args.tier_size, tier_distance=args.tier_distance,
@@ -834,7 +893,7 @@ class CODER(nn.Module):
             over the sequence of query term embeddings in the output of the query encoder
         :param transform_doc_emb: if set, document embeddings will be linearly projected to match `d_model`
         :param bias_regul_coeff: coefficient for bias regularization term in the total loss
-        :param bias_regul_cutoff:
+        :param bias_regul_cutoff: how many top-scored documents to consider for bias regularization
         """
         super(CODER, self).__init__()
 
@@ -904,7 +963,7 @@ class CODER(nn.Module):
         self.aux_loss_coeff = aux_loss_coeff
         
         self.bias_regul_coeff = bias_regul_coeff
-        self.bias_regul_cutoff = bias_regul_cutoff
+        self.bias_regul_module = BiasRegularizationLoss(bias_regul_cutoff=bias_regul_cutoff)
 
     def get_scoring_module(self, scoring_mode, query_emb_aggregation, temperature):
 
@@ -945,7 +1004,7 @@ class CODER(nn.Module):
         :param  doc_attention_mat_mask: (num_docs, num_docs) float additive mask for the decoder sequence (optional).
                     This is for causality, and if FloatTensor, can be directly added on top of the attention matrix.
                     If BoolTensor, positions with ``True`` are ignored, while ``False`` values will be considered.
-        :param  doc_neutscore: (batch_size, num_docs) sequence of document neutrality scores to calculate fairness.
+        :param  doc_neutscore: (batch_size, num_docs) tensor of document neutrality scores to calculate fairness.
         :param  labels: (batch_size, num_docs) optional tensor, if provided, the loss will be computed. 
             Depends on `self.loss_module.formatting`: 'indices' or 'scores'.
             If 'scores', it is a float tensor with the same formatting as predictions: for each row, each position 
@@ -1003,31 +1062,19 @@ class CODER(nn.Module):
         else:
             rel_scores = predictions.squeeze()  # (batch_size, num_docs) relevance scores
             
-        # Fairness regularization term  # TODO: wrap in a separate function
-        bias_regul_term = None
-        if doc_neutscore is not None:
-
-            _cutoff = np.min([self.bias_regul_cutoff, doc_neutscore.shape[1]])
-
-            _indices_sorted = torch.argsort(rel_scores, dim=1, descending=True)
-            _indices_sorted[_indices_sorted < _cutoff] = -1
-            _indices_sorted[_indices_sorted != -1] = 0
-            _indices_sorted[_indices_sorted == -1] = 1
-            _indices_mask = doc_neutscore.new_zeros(doc_neutscore.shape)    
-            _indices_mask[_indices_sorted == 0] = float("-Inf")
-
-            doc_neutscore_probs = torch.nn.Softmax(dim=1)(doc_neutscore + _indices_mask)
-            rel_scores_logprobs = torch.nn.LogSoftmax(dim=1)(rel_scores + _indices_mask)
-
-            bias_regul_term = torch.nn.KLDivLoss(reduction='batchmean')(rel_scores_logprobs, doc_neutscore_probs)
 
         # Compute loss
         if labels is not None:
             loss = self.loss_module(rel_scores, labels, padding_mask=~doc_padding_mask)  # loss is scalar tensor
-
-            if self.aux_loss_module is not None and (self.aux_loss_coeff > 0):  # add auxiliary loss, if specified
+            # add auxiliary loss, if specified
+            if self.aux_loss_module is not None and (self.aux_loss_coeff > 0):
                 loss += self.aux_loss_coeff * self.aux_loss_module(rel_scores, labels, padding_mask=~doc_padding_mask)
-            if bias_regul_term is not None:
+            
+            # add fairness regularization term, if specified
+            if doc_neutscore is not None:
+                
+                bias_regul_term = self.bias_regul_module(rel_scores, labels=doc_neutscore, padding_mask=~doc_padding_mask)
+                
                 if self.bias_regul_coeff < 0:
                     loss = rel_scores.new([0])[0]
                     loss += - self.bias_regul_coeff * bias_regul_term

reciprocal_compute.py
@@ -14,6 +14,7 @@ import json
 import pickle
 import builtins
 
+import h5py
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -498,7 +499,7 @@ def recip_NN_rerank(args):
     if args.write_to_file:
         start_time = time.perf_counter()
         logger.info(f"Writing ranks to {args.out_filepath} ...")
-        utils.write_predictions(args.out_filepath, reranked_scores, args.write_to_file)
+        utils.write_predictions(args.out_filepath, reranked_scores, format=args.write_to_file)
         global write_results_times
         write_results_times.update(time.perf_counter() - start_time)
 
@@ -561,12 +562,17 @@ def extend_relevances(args):
     logger.info("Max memory usage: {} MB".format(int(np.ceil(utils.get_max_memory_usage()))))
     
     if args.write_to_file:
+        del recipn
         logger.info(f"Writing recomputed labels to {args.out_filepath} ...")
         start_time = time.perf_counter()
-        with open(args.out_filepath, args.mode) as out_file:
-            if args.write_to_file == 'pickle':
+        if args.write_to_file == 'hdf5':
+            with h5py.File(args.out_filepath, args.mode) as out_file:
+                utils.write_dict_hdf5(out_file, recomputed_labels, dtype='float16')
+        elif args.write_to_file == 'pickle':
+            with open(args.out_filepath, args.mode) as out_file:
                 pickle.dump(recomputed_labels, out_file, protocol=pickle.HIGHEST_PROTOCOL)
-            else:
+        else:
+            with open(args.out_filepath, args.mode) as out_file:
                 for qid, doc2score in recomputed_labels.items():
                     for i, (docid, score) in enumerate(doc2score.items()):
                         out_file.write(f"{qid}\tQ0\t{docid}\t{score:.8f}\n")
@@ -608,7 +614,12 @@ def setup(args):
     args = utils.dict2obj(config)
     
     # Rest of setup (no need to store in configuration file)
-    suffix = '_labels' if config['task'] == 'smooth_labels' else ''
+    suffix = ''  # part of filename that is added to output file
+    if args.task == 'smooth_labels':
+        suffix = '_labels'
+        if args.write_to_file is None:
+            args.write_to_file = 'hdf5' # force writing to file, otherwise task is meaningless
+    
     if args.write_to_file:
         args.out_filepath = os.path.join(output_dir, args.out_name + f'_top{args.hit}' + suffix + '.' + args.write_to_file)
         args.mode = 'wb' if args.write_to_file == 'pickle' else 'w'
@@ -675,8 +686,8 @@ def run_parse_args():
                     
     parser.add_argument('--records_file', default='./records.xls', 
                         help='Excel file keeping best records of all experiments')
-    parser.add_argument("--write_to_file", type=str, choices=['tsv', 'pickle'], default=None,
-                        help="If set, predictions (scores per document for each query) will be written to a .tsv file")
+    parser.add_argument("--write_to_file", type=str, choices=['hdf5', 'tsv', 'pickle'], default=None,
+                        help="If set, predictions (scores per document for each query) will be written to a file of the specified format")
     parser.add_argument("--doc_embedding_dir", type=str,
                         help="Directory containing the memmap files corresponding to document embeddings.")
     parser.add_argument("--query_embedding_dir", type=str,

utils.py
@@ -16,10 +16,12 @@ import pickle
 from matplotlib import pyplot as plt
 from typing import Dict, List, Tuple
 import csv
+import glob
 
 from tqdm import tqdm
 import numpy as np
 import torch
+import h5py
 import xlrd
 import xlwt
 import xlutils.copy
@@ -87,14 +89,14 @@ def get_relevances(gt_relevant, candidates, max_docs=None, relevant_at_level=1):
     return [gt_relevant[candid] if (candid in gt_relevant) and gt_relevant[candid] >= relevant_at_level else 0 for candid in candidates[:max_docs]]
 
 
-# NOTE: except for MRR, metrics differ from official TREC. Use get_retrieval_metrics instead.
+# NOTE: except for MRR and Recall, metrics differ from official TREC. Use get_retrieval_metrics instead.
 def calculate_metrics(relevances, num_relevant, k):
     eval_metrics = OrderedDict([('MRR', metrics.mean_reciprocal_rank(relevances)),
                                 ('MRR@{}'.format(k), metrics.mean_reciprocal_rank(relevances, k)),
                                 # ('MAP', metrics.mean_average_precision(relevances)),
                                 # ('MAP@{}'.format(k), metrics.mean_average_precision(relevances, k)),
                                 # ('MAP@R', metrics.mean_average_precision_at_r(relevances, num_relevant)),
-                                # ('Recall@{}'.format(k), metrics.recall_at_k(relevances, num_relevant, k)),
+                                ('Recall@{}'.format(k), metrics.recall_at_k(relevances, num_relevant, k)),
                                 # ('nDCG', np.mean([metrics.ndcg_at_k(rel) for rel in relevances])),
                                 # ('nDCG@{}'.format(k), np.mean([metrics.ndcg_at_k(rel, k) for rel in relevances]))
                                 ])
@@ -106,7 +108,7 @@ def write_predictions(filepath, predictions, format='tsv', score_type=builtins.f
 
     :param filepath: output file path
     :param predictions: dict of method's predictions per query, {str qID: {str pID: float score}}
-    :param format: 'pickle' or 'tsv'
+    :param format: 'pickle', 'tsv' or 'hdf5'
     :param score_type: Only for format 'tsv': the type of the written scores (int or float).
     :param is_qrels: Only for format 'tsv': if True, will write 'tsv' file in qrels format with a column 'Q0' and no rank.
     """
@@ -122,9 +124,45 @@ def write_predictions(filepath, predictions, format='tsv', score_type=builtins.f
     elif format == 'pickle':
         with open(filepath, 'wb') as out_file:
             pickle.dump(predictions, out_file, protocol=pickle.HIGHEST_PROTOCOL)
+    elif format == 'hdf5':
+        with h5py.File(filepath, 'w') as out_file:
+            write_dict_hdf5(out_file, predictions, dtype='float16')
     else:
         logger.debug("format '{}'; will not write output file")
     return
+
+
+def write_dict_hdf5(group, d, dtype='float16'):
+    """Write a dictionary to an HDF5 group; `group` starts as a file object,
+    but subgroups will be created if necessary, to recursively write nested dictionaries.
+
+    :param group: HDF5 group object (initially a file object)
+    :param d: the dictionary to be written to disk
+    """
+    for key, value in d.items():
+        if isinstance(value, dict):
+            subgroup = group.create_group(key)
+            write_dict_hdf5(subgroup, value, dtype)
+        else:
+            # group[key] = value  # this is used to write generic nested dictionaries, but doesn't specify dtype
+            group.create_dataset(key, data=value, dtype=dtype)
+    return
+
+
+def read_dict_hdf5(group):
+    """Read a dictionary from an HDF5 group; `group` starts as a file object,
+    but existing subgroups will be extracted, to recursively read nested dictionaries.
+
+    :param group: HDF5 group object (initially a file object)
+    :return: the dictionary holding the data
+    """
+    d = {}
+    for key, value in group.items():
+        if isinstance(value, h5py.Group):
+            d[key] = read_dict_hdf5(value)
+        else:
+            d[key] = value[()]
+    return d
 
 
 # DEPRECATED
@@ -449,7 +487,7 @@ def plot_rank_histogram(qrels, pred_scores, base_pred_scores=None, include_groun
     return freqs, bin_edges
 
 
-def plot_rank_barplot(qrels, pred_scores, base_pred_scores=None, include_ground_truth='all', bins=None, relevance_thr=1, save_as='ranks.pdf'):
+def plot_rank_barplot(qrels, pred_scores, base_pred_scores=None, include_ground_truth='all', bins=None, relevance_thr=1, orient_bars='vertical', save_as='ranks.pdf'):
     """
     Plots a histogram with the rank that ground-truth relevant documents ain `qrels` achieved 
     based on the predicted model scores in `pred_scores`. The difference with `plot_rank_histogram` is that
@@ -462,8 +500,10 @@ def plot_rank_barplot(qrels, pred_scores, base_pred_scores=None, include_ground_
         If given, it will be superimposed on the plot for reference.
     :param include_ground_truth: if 'all', the histogram will include the ranks of all g.t. documents per query
         (naturally, only one of them can have rank 1). If 'top', the histogram will only include the ranks of the top-scored g.t. document.
+    :param bins: iterable of length 69; the bins to use for the histogram. If None, the default bins will be used.
     :param relevance_thr: the relevance score that a document in qrels must have in order to be g.t. considered relevant 
-    :param save_as: _description_, defaults to 'ranks.pdf'
+    :param orient_bars: orientation of bars; 'vertical' or 'horizontal', defaults to 'vertical'.
+    :param save_as: filepath to save figure, defaults to 'ranks.pdf'
     :return: (freqs, bin_edges) tuple of counts/frequencies and bin edges of pred_scores historgram
     """
     
@@ -478,12 +518,15 @@ def plot_rank_barplot(qrels, pred_scores, base_pred_scores=None, include_ground_
         if type(base_pred_scores) is dict:
             base_pred_ranks = np.array(list(chain.from_iterable(get_ranks_of_top(qrels, base_pred_scores))), dtype=np.int16)
     
-    bins = list(range(1, 50)) + list(range(50, 1050, 50))
+    if bins is None:
+        bins = list(range(1, 50)) + list(range(50, 1050, 50))
     bin_labels = ["[{}, {})".format(bins[i], bins[i + 1]) for i in range(len(bins) - 1)] #+ ["[{}, inf)".format(bins[-1])]
     
-    fig, ax = plt.subplots(figsize=(10, 30))
+    if orient_bars == 'horizontal':
+        fig, ax = plt.subplots(figsize=(10, 30))
+    else:
+        fig, ax = plt.subplots(figsize=(30, 10))
     
-    # plt.figure(figsize=(10, 30))
     if base_pred_scores is not None:
         if type(base_pred_scores) is dict:
             freqs, bin_edges = np.histogram(base_pred_ranks, bins=bins)
@@ -491,37 +534,56 @@ def plot_rank_barplot(qrels, pred_scores, base_pred_scores=None, include_ground_
             freqs, bin_edges = base_pred_scores
         orig_freqs = freqs
         # plt.hist(bin_edges[:-1], bins=bin_edges, weights=freqs, alpha=0.2, color='red', edgecolor='gray', label='Original')
-        ax.barh(np.arange(len(freqs)), freqs, height=1, align='edge', alpha=0.2, color='red', edgecolor='gray', label='Original')
+        if orient_bars == 'horizontal':
+            ax.barh(np.arange(len(freqs)), freqs, height=1, align='edge', alpha=0.2, color='red', edgecolor='gray', label='Original')
+        else:
+            ax.bar(np.arange(len(freqs)), freqs, width=1, align='edge', alpha=0.2, color='red', edgecolor='gray', label='Original')
     
     freqs, bin_edges = np.histogram(pred_ranks, bins=bins)
     # plt.hist(bin_edges[:-1], bins=bin_edges, weights=freqs, alpha=0.2, color='blue', edgecolor='gray', label='Reranked')
-    y_pos = np.arange(len(freqs))
-    rects = ax.barh(y_pos, freqs, height=1, align='edge', alpha=0.2, color='blue', edgecolor='gray', label='Reranked')
-    ax.set_yticks(y_pos)
-    ax.set_yticklabels(bin_labels)
-    ax.invert_yaxis()  # labels read top-to-bottom
-    ax.set_xlabel('Counts')
-    ax.set_title(title_str)
-
-    # Label with specially formatted floats
+    
+    # Prepare labels for the bars
     if base_pred_scores is None:
         label_vals = freqs
         # ax.bar_label(hbars, fmt='%d', padding=5)  # Requires matplotlib 3.4.0+
-        xlim = max(freqs)
+        axis_lim = max(freqs)
     else:
         label_vals = freqs - orig_freqs
-        # ax.bar_label(hbars, fmt='%d', labels=labels, padding=10)
-        xlim = max(max(freqs), max(orig_freqs))
+        # ax.bar_label(hbars, fmt='%d', labels=labels, padding=10)  # Requires matplotlib 3.4.0+
+        axis_lim = max(max(freqs), max(orig_freqs))
     
-    # Put labels on the right side of bars    
-    for rect, label_val in zip(rects, label_vals):
-        width = rect.get_width()
-        color = 'red' if label_val < 0 else 'blue'
-        ax.text(width + 0.1*xlim, rect.get_y() + rect.get_height() / 2, str(label_val), ha="center", va="center", color=color)
+    axis_pos = np.arange(len(freqs))
     
-    ax.set_xlim(right=1.2*xlim)  # adjust xlim to fit labels
+    if orient_bars == 'horizontal':
+        rects = ax.barh(axis_pos, freqs, height=1, align='edge', alpha=0.2, color='blue', edgecolor='gray', label='Reranked')
+        ax.set_yticks(axis_pos)
+        ax.set_yticklabels(bin_labels)
+        ax.invert_yaxis()  # labels read top-to-bottom
+        ax.set_xlabel('Counts')
+
+        # Put labels on the right side of bars    
+        for rect, label_val in zip(rects, label_vals):
+            width = rect.get_width()
+            color = 'red' if label_val < 0 else 'blue'
+            ax.text(width + 0.1*axis_lim, rect.get_y() + rect.get_height() / 2, str(label_val), ha="center", va="center", color=color)
+        
+        ax.set_xlim(right=1.2*axis_lim)  # adjust xlim to fit labels
+    else:
+        rects = ax.bar(axis_pos, freqs, width=1, align='edge', alpha=0.2, color='blue', edgecolor='gray', label='Reranked')
+        ax.set_xticks(axis_pos)
+        ax.set_xticklabels(bin_labels, rotation=90)
+        ax.set_ylabel('Counts')
+        
+        # Put labels on top of bars
+        for rect, label_val in zip(rects, label_vals):
+            height = rect.get_height()
+            color = 'red' if label_val < 0 else 'blue'
+            ax.text(rect.get_x() + rect.get_width() / 2, height + 0.1*axis_lim, str(label_val), ha="center", va="center", color=color)
+        
+        ax.set_ylim(top=1.2*axis_lim)  # adjust ylim to fit labels
+   
     ax.legend()
-    
+    ax.set_title(title_str)
     plt.savefig(save_as)
     return freqs, bin_edges
 
@@ -814,14 +876,14 @@ def get_retrieval_metrics(results, qrels, cutoff_values=(1, 3, 5, 10, 100, 1000)
     start_time = time.perf_counter()
     # Evaluate using pytrec_eval for comparison with BEIR benchmarks
     # Returns dictionaries with metrics for each cut-off value, e.g. ndcg["NDCG@{k}".format(cutoff_values[0])] == 0.3
-    #metric_dicts = EvaluateRetrieval.evaluate(qrels, results, cutoff_values, ignore_identical_ids=False, verbose=verbose)  # tuple of metrics dicts (ndct, ...) #TODO: RESTORE
+    metric_dicts = EvaluateRetrieval.evaluate(qrels, results, cutoff_values, ignore_identical_ids=False, verbose=verbose)  # tuple of metrics dicts (ndct, ...) #TODO: RESTORE
     mrr = EvaluateRetrieval.evaluate_custom(qrels, results, cutoff_values, 'MRR', relevance_level=relevance_level, verbose=verbose)
     metrics_time = time.perf_counter() - start_time
     logger.info("Time to calculate performance metrics: {:.3f} s".format(metrics_time))
     
     # Merge all dicts into a single OrderedDict and sort by k for guaranteed consistency
     perf_metrics = OrderedDict()  # to also work with Python 3.6
-    for met_dict in (mrr, ):#+ metric_dicts: #TODO: RESTORE
+    for met_dict in (mrr, ) + metric_dicts: #TODO: RESTORE
         perf_metrics.update(sorted(met_dict.items(), key=lambda x: 0 if not '@' in x[0] else int(x[0].split('@')[1])))
 
     return perf_metrics
@@ -1021,3 +1083,57 @@ def load_predictions(filepath, seperator=None):
             qid_to_candidate_passages[qid][pid] = score
     
     return qid_to_candidate_passages
+
+
+def merge_qrels_dictionaries(dicts, overwrite_queries=False):
+    """Merge multiple qrel dictionaries, in a way that new documents can be added to existing shared queries,
+    and if for the same query a passage is present in multiple dictionaries, the value from the last will be used.
+    
+    :param dicts: iterable of qrels dictionaries to merge. Each is a dict of the form {qid: {pid: rel}}
+    :param overwrite_queries: if True, queries that are present in multiple dictionaries will be overwritten by the last;
+        otherwise, they will be merged such that new passages can be added to existing queries (and shared passages will be overwritten).
+    """
+    merged = defaultdict(dict)
+    
+    if overwrite_queries:
+        for d in dicts:
+            merged.update(d)
+    else:
+        for d in dicts:
+            for qid, pid2rel in d.items():
+                merged[qid].update(pid2rel)
+    return merged
+
+
+def load_qrels_from_pickles(path_pattern, overwrite_queries=False):
+    """Load qrels dictionaries from multiple pickle files, and merge them into a single dictionary.
+    :param path_pattern: path pattern to pickle files, e.g. "/path/to/qrels/*.pickle"
+    :param overwrite_queries: if True, queries that are present in multiple dictionaries will be overwritten by the last;
+        otherwise, they will be merged such that new passages can be added to existing queries (and shared passages will be overwritten).
+    """
+    qrels = defaultdict(dict)
+    for path in glob.glob(path_pattern):
+        with open(path, 'rb') as f:
+            if overwrite_queries:
+                qrels.update(pickle.load(f))
+            else:
+                for qid, pid2rel in pickle.load(f).items():
+                    qrels[qid].update(pid2rel)
+    return qrels
+
+
+def load_qrels_from_hdf5(path_pattern, overwrite_queries=False):
+    """Load qrels dictionaries from multiple HDF5 files, and merge them into a single dictionary.
+    :param path_pattern: path pattern to HDF5 files, e.g. "/path/to/qrels/*.hdf5"
+    :param overwrite_queries: if True, queries that are present in multiple dictionaries will be overwritten by the last;
+        otherwise, they will be merged such that new passages can be added to existing queries (and shared passages will be overwritten).
+    """
+    qrels = defaultdict(dict)
+    for path in glob.glob(path_pattern):
+        with h5py.File(path, "r") as f:
+            if overwrite_queries:
+                qrels.update(read_dict_hdf5(f))
+            else:
+                for qid, pid2rel in read_dict_hdf5(f).items():
+                    qrels[qid].update(pid2rel)
+    return qrels